696cc444a51cf78f4e025d7e3250b655added957
Laura Abbott <lauramargaretabbott@gmail.com>
2021-05-09 13:56:52 +0100

    Added API for Weather Icon
    Weather icon now pulling in from the API

--- src/app.js
+++ src/app.js
@@ -23,21 +23,27 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#windSpeed");
   let dateElement = document.querySelector("#date");
+  let weatherIconElement = document.querySelector("#weatherIcon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  weatherIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  weatherIconElement.setAttribute("alt".response.data.weather[0].description);
 }
-let city = "London";
+let city = "Lisbon";
 let apiKey = "ba5ecf513fd2fbe8a7177219d1410c1b";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
